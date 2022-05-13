7fea80a2d4b49a2929b459b3c75f226558cafecf
Arnold Sobanski <arnold.sobanski@l4g.eu>
2022-05-13 18:29:43 +0100

    update gh connection name to service and env

--- codestar.tf
+++ codestar.tf
@@ -1,4 +1,4 @@
 resource "aws_codestarconnections_connection" "github" {
-  name          = "github-connection"
+  name          = "${var.labels.tags.Environment}-${var.labels.tags.Service}"
   provider_type = "GitHub"
 }
