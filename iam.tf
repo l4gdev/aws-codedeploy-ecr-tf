d6503ed36dff11116a95e08a4420dbad35d89171
Arnold Sobanski <arnold.sobanski@l4g.eu>
2023-02-14 14:58:13 +0000

    fix tf run only

--- iam.tf
+++ iam.tf
@@ -5,14 +5,14 @@ data "aws_s3_bucket" "bucket_name" {
 resource "aws_iam_role" "codepipeline_role" {
   name = lower("${var.environment_name}-${var.application_name}-codepipeline-role")
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version : "2012-10-17",
+    Statement : [
       {
-        "Effect" : "Allow",
+        Effect : "Allow",
         "Principal" : {
           "Service" : "codepipeline.amazonaws.com"
         },
-        "Action" : "sts:AssumeRole"
+        Action : "sts:AssumeRole"
       }
     ]
     }
@@ -25,11 +25,11 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
   role = aws_iam_role.codepipeline_role.id
   policy = jsonencode(
     {
-      "Version" : "2012-10-17",
-      "Statement" : [
+      Version : "2012-10-17",
+      Statement : concat([
         {
-          "Effect" : "Allow",
-          "Action" : [
+          Effect : "Allow",
+          Action : [
             "s3:GetObject",
             "s3:GetObjectVersion",
             "s3:GetBucketVersioning",
@@ -37,22 +37,22 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
             "s3:PutObjectAcl",
             "s3:PutObject"
           ],
-          "Resource" : [
+          Resource : [
             data.aws_s3_bucket.bucket_name.arn,
             "${data.aws_s3_bucket.bucket_name.arn}/*"
           ]
         },
         {
-          "Effect" : "Allow",
-          "Action" : [
+          Effect : "Allow",
+          Action : [
             "codebuild:BatchGetBuilds",
             "codebuild:StartBuild"
           ],
-          "Resource" : "*"
+          Resource : "*"
         },
         {
-          "Effect" : "Allow",
-          "Action" : [
+          Effect : "Allow",
+          Action : [
             "codecommit:GetBranch",
             "codecommit:GetRepository",
             "codecommit:GetCommit",
@@ -61,46 +61,39 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
             "codecommit:GetUploadArchiveStatus",
             "codecommit:CancelUploadArchive"
           ],
-          "Resource" : "arn:aws:codecommit:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+          Resource : "arn:aws:codecommit:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
         },
-        #        {
-        #          "Effect" : "Allow",
-        #          "Action" : [
-        #            "kms:Decrypt",
-        #            "kms:Encrypt",
-        #            "kms:GenerateDataKey"
-        #          ],
-        #          "Resource" : var.aft_key_arn
-        #        },
         {
-          "Effect" : "Allow",
-          "Action" : "codestar-connections:UseConnection",
-          "Resource" : "*"
-          }, {
-          "Effect" : "Allow",
-          "Action" : [
+          Effect : "Allow",
+          Action : "codestar-connections:UseConnection",
+          Resource : "*"
+        },
+
+        ],
+        length(aws_codebuild_project.build) > 0 ? [{
+          Effect : "Allow",
+          Action : [
             "codebuild:StartBuild"
           ],
-          "Resource" : [for build in aws_codebuild_project.build : "arn:aws:codebuild:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:project/${build.name}"]
-        }
-      ]
+          Resource : [for build in aws_codebuild_project.build : "arn:aws:codebuild:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:project/${build.name}"]
+      }] : [])
     }
-
   )
 }
+
 
 resource "aws_iam_role" "build" {
   name = lower("${var.environment_name}-${var.application_name}-codebuild-role")
   tags = var.tags
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version : "2012-10-17",
+    Statement : [
       {
-        "Effect" : "Allow",
+        Effect : "Allow",
         "Principal" : {
           "Service" : "codebuild.amazonaws.com"
         },
-        "Action" : "sts:AssumeRole"
+        Action : "sts:AssumeRole"
       }
     ]
     }
@@ -112,19 +105,19 @@ resource "aws_iam_role_policy" "codebuild_role" {
   role = aws_iam_role.build.id
 
   policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version : "2012-10-17",
+    Statement : [
       {
-        "Effect" : "Allow",
-        "Resource" : "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*",
-        "Action" : [
+        Effect : "Allow",
+        Resource : "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*",
+        Action : [
           "logs:CreateLogStream",
           "logs:PutLogEvents"
         ]
       },
       {
-        "Effect" : "Allow",
-        "Action" : [
+        Effect : "Allow",
+        Action : [
           "ec2:CreateNetworkInterface",
           "ec2:DescribeDhcpOptions",
           "ec2:DescribeNetworkInterfaces",
@@ -133,20 +126,20 @@ resource "aws_iam_role_policy" "codebuild_role" {
           "ec2:DescribeSecurityGroups",
           "ec2:DescribeVpcs"
         ],
-        "Resource" : "*"
+        Resource : "*"
       },
       {
-        "Effect" : "Allow",
-        "Action" : [
+        Effect : "Allow",
+        Action : [
           "ec2:CreateNetworkInterfacePermission"
         ],
-        "Resource" : [
+        Resource : [
           "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:network-interface/*"
         ]
       },
       {
-        "Effect" : "Allow",
-        "Action" : [
+        Effect : "Allow",
+        Action : [
           "s3:GetObject",
           "s3:GetObjectVersion",
           "s3:GetBucketVersioning",
@@ -154,33 +147,33 @@ resource "aws_iam_role_policy" "codebuild_role" {
           "s3:PutObjectAcl",
           "s3:PutObject"
         ],
-        "Resource" : [
+        Resource : [
           data.aws_s3_bucket.bucket_name.arn,
           "${data.aws_s3_bucket.bucket_name.arn}/*"
         ]
       },
       #      {
-      #        "Effect" : "Allow",
-      #        "Action" : [
+      #        Effect : "Allow",
+      #        Action : [
       #          "kms:Decrypt",
       #          "kms:Encrypt",
       #          "kms:GenerateDataKey"
       #        ],
-      #        "Resource" : "${var.aft_key_arn}"
+      #        Resource : "${var.aft_key_arn}"
       #      },
       {
-        "Effect" : "Allow",
-        "Action" : [
+        Effect : "Allow",
+        Action : [
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource" : [
+        Resource : [
           "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/*"
         ]
       },
       {
-        "Effect" : "Allow",
-        "Action" : [
+        Effect : "Allow",
+        Action : [
           "codecommit:GetBranch",
           "codecommit:GetRepository",
           "codecommit:GetCommit",
@@ -189,22 +182,22 @@ resource "aws_iam_role_policy" "codebuild_role" {
           "codecommit:GetUploadArchiveStatus",
           "codecommit:CancelUploadArchive"
         ],
-        "Resource" : "arn:aws:codecommit:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+        Resource : "arn:aws:codecommit:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
       },
       {
-        "Effect" : "Allow",
-        "Action" : [
+        Effect : "Allow",
+        Action : [
           "sts:AssumeRole"
         ],
-        "Resource" : local.roles_allowed_to_assume
+        Resource : local.roles_allowed_to_assume
       },
       {
-        "Effect" : "Allow",
-        "Action" : [
+        Effect : "Allow",
+        Action : [
           "ecr:*"
         ],
         //TODO limit perms to single repo only
-        "Resource" : [
+        Resource : [
           "*"
         ]
       }
